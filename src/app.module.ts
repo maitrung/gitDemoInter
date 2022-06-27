d746b8ee904cc1fcc40fae576e007995e25023f9
maitrung <maitrungkienat@gmail.com>
2022-06-27 19:13:30 +0700

    27062022

---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common'; 
+import { AppController } from './app.controller';
+ import { AppService } from './app.service'; 
+ import { MongooseModule } from '@nestjs/mongoose'; 
+import { BlogModule } from './blog/blog.module';
+@Module({   
+      imports: [ MongooseModule.forRoot('mongodb://localhost/unit1',{useNewUrlParser: true }), BlogModule,],   
+      controllers: [AppController],   
+      providers: [AppService], }) export class AppModule { }
---
+++ src/blog/blog.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Res, HttpStatus, Param, NotFoundException, Post, Body, Query, Put, Delete } from '@nestjs/common'; 
+import { BlogService } from './blog.service'; 
+import { CreatePostDTO } from './dto/create-post.dto'; 
+import { ValidateObjectId } from '../blog/shared/pipes/validate-object-id.pipes';   
+@Controller('blog') 
+export class BlogController {constructor(private blogService: BlogService) { }      
+@Get('posts')    
+ async getPosts(@Res() res) 
+{const posts = await this.blogService.getPosts();         
+return res.status(HttpStatus.OK).json(posts);}
+@Get('post/:postID')
+async getPost(@Res() res, @Param('postID', new ValidateObjectId()) postID) 
+{         
+const post = await this.blogService.getPost(postID);
+if (!post) throw new NotFoundException('Post does not exist!');  
+return res.status(HttpStatus.OK).json(post);}
+@Post('/post')     
+async addPost(@Res() res, @Body() title:string,description:string,body:string,author:string,date_posted:string)
+{const newPost = await this.blogService.addPost(title,description,body,date_posted,author);        
+return res.status(HttpStatus.OK).json(
+     {message: "Post has been submitted successfully!",             
+     post: newPost})} } 
